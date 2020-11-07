0b965f2b776c5688837aaf234bdb4f26b5c3a5e6
Frederik Brandt <frederiksbrandt@gmail.com>
2020-11-07 12:33:54 +0100

    gitflow-feature-stash: footer

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,6 +1,7 @@
 <template>
-  <footer class="footer container">
-    <h2 class="footer__title">Footer</h2>
-  </footer>
+    <footer class="footer container">
+        <h2 class="footer__title">Footer</h2>
+        <p>test</p>
+    </footer>
 </template>
 <script src="./footer.ts"></script>
